8dd2185b0d803b8ff254b1610874c2b9c0a30b14
chibbs2445 <62122089+chibbs2445@users.noreply.github.com>
2023-04-20 12:52:10 -0500

    Create index-delivery-customization.js

---
+++ index-delivery-customization.js
@@ -0,0 +1,49 @@
+// @ts-check
+// Use JSDoc annotations for type safety
+/**
+* @typedef {import("../generated/api").InputQuery} InputQuery
+* @typedef {import("../generated/api").FunctionResult} FunctionResult
+* @typedef {import("../generated/api").Operation} Operation
+*/
+
+const NO_CHANGES = {
+  operations: [],
+};
+
+// The @shopify/shopify_function package will use the default export as your function entrypoint
+export default 
+/**
+* @param {InputQuery} input
+* @returns {FunctionResult}
+*/
+  (input) => {
+
+    const targets = input.cart.lines
+    .filter(line => line.merchandise.__typename == "ProductVariant")
+    .filter(line => { 
+      const variant = /** @type {ProductVariant} */ (line.merchandise);
+      return variant.product.hasPreorder
+    })
+
+    if (!targets.length) {
+      // You can use STDERR for debug logs in your function
+      return NO_CHANGES;
+    }
+
+    let toHide = input.cart.deliveryGroups
+      // Collect the delivery options from these groups
+      .flatMap(group => group.deliveryOptions)
+      .filter(option => option.title == "Economy")
+      // Construct a rename operation for each, adding the message to the option title
+      .map(option => /** @type {Operation} */({
+        hide: {
+          deliveryOptionHandle: option.handle,
+        }
+      }));
+
+    // The @shopify/shopify_function package applies JSON.stringify() to your function result
+    // and writes it to STDOUT
+    return {
+      operations: toHide
+    };
+  };
